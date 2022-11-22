6b6cb8e0d2baebf2ed7d68e44f378487536bd0c9
matheusjferreira <matheusferreira772@tutanota.com>
2022-11-22 14:31:31 -0300

    fix: update request get data

--- lib/modules/search_cep/repositories/repository.dart
+++ lib/modules/search_cep/repositories/repository.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+
 import '../../../shared/services/api.dart';
 import '../models/cep_model.dart';
 
@@ -7,11 +9,16 @@ import 'package:http/http.dart' as http;
 
 class HomeRepository {
   Future getData(String cep) async {
-    final response = await http.get(Uri.parse('${ApiCep.urlBase}$cep/json'));
-    if (response.statusCode == 200) {
-      return CepModel.fromJson(jsonDecode(response.body));
-    } else {
-      return 'Servidor indisponível, tente novamente mais tarde';
+    try {
+      http.Response response = await http.get(
+        Uri.parse('${ApiCep.urlBase}$cep/json'),
+      );
+      if (response.statusCode == 200) {
+        return CepModel.fromJson(jsonDecode(response.body));
+      }
+    } catch (e) {
+      debugPrint('$e');
+      return 'Servidor indisponível, tente novamente mais tarde...';
     }
   }
 }
